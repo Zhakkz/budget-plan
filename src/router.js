7594482517ec83fa062d4ed7aad58f62c628651c
Aday <aday0512@mail.ru>
2019-08-09 22:03:42 +0600

    Добавил страницы и настроил их роут

--- src/router.js
+++ src/router.js
@@ -11,15 +11,75 @@ export default new Router({
     {
       path: '/',
       name: 'home',
-      component: Home
+      component: Home,
+      meta:{
+        layout:'main'
+      },
     },
     {
-      path: '/about',
-      name: 'about',
-      // route level code-splitting
-      // this generates a separate chunk (about.[hash].js) for this route
-      // which is lazy-loaded when the route is visited.
-      component: () => import(/* webpackChunkName: "about" */ './views/About.vue')
+      path:'/login',
+      name:'login',
+      meta:{
+        layout:'empty'
+      },
+      component: () => import('./views/Login.vue')
+    },
+    {
+      path:'/categories',
+      name:'categories',
+      meta:{
+        layout:'main'
+      },
+      component: () => import('./views/Categories.vue')
+    },
+    {
+      path:'/register',
+      name:'register',
+      meta:{
+        layout:'empty'
+      },
+      component: () => import('./views/Register.vue')
+    },
+    {
+      path:'/detail-record',
+      name:'detail-record',
+      meta:{
+        layout:'main'
+      },
+      component: () => import('./views/Detail-record.vue')
+    },
+    {
+      path:'/history',
+      name:'history',
+      meta:{
+        layout:'main'
+      },
+      component: () => import('./views/History.vue')
+    },
+    {
+      path:'/planning',
+      name:'planning',
+      meta:{
+        layout:'main'
+      },
+      component: () => import('./views/Planning.vue')
+    },
+    {
+      path:'/profile',
+      name:'profile',
+      meta:{
+        layout:'main'
+      },
+      component: () => import('./views/Profile.vue')
+    },
+    {
+      path:'/record',
+      name:'record',
+      meta:{
+        layout:'main'
+      },
+      component: () => import('./views/Record.vue')
     }
+
   ]
 })
