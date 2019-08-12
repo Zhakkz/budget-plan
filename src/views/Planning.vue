487351b9dfe832d51e2b487dd74cb2ef8576f1da
Aday <aday0512@mail.ru>
2019-08-12 16:09:56 +0600

    Закончил блок планирование

--- src/views/Planning.vue
+++ src/views/Planning.vue
@@ -3,19 +3,23 @@
             <div>
                 <div class="page-title">
                   <h3>Планирование</h3>
-                  <h4>12 212</h4>
+                  <h4>{{info.bill | currency('RUB')}}</h4>
                 </div>
 
-                <section>
-                  <div>
+                <Loader v-if="loading"></Loader>
+                <p class="center" v-else-if="!categories.length">категорию пока НЕТ <router-link to="/categories">Добавить новую категорию</router-link></p>
+  
+                <section v-else>
+                  <div v-for="c in categories" :key="c.id">
                     <p>
-                      <strong>Девушка:</strong>
-                      12 122 из 14 0000
+                      <strong>{{c.title}}</strong>
+                      {{c.spend | currency('RUB')}} из {{c.limit | currency('RUB')}}
                     </p>
                     <div class="progress" >
                       <div
                           class="determinate green"
-                          style="width:40%"
+                          :class="[c.progressColor]"
+                          :style="{'width': c.progressPercent + '%'}"
                       ></div>
                     </div>
                   </div>
@@ -23,3 +27,48 @@
               </div>
       </div>
 </template>
+
+<script>
+import {mapGetters} from 'vuex'
+
+export default {
+  name: 'planning',
+  data() {
+    return {
+      loading: true,
+      categories: []
+    }
+  },
+  computed: {
+    ...mapGetters(['info'])
+  },
+  async mounted() {
+        const records = await this.$store.dispatch('fetchRecords')
+        const categories = await this.$store.dispatch('fetchCategories')
+
+        this.categories = categories.map( i => {
+          const spend = records
+          .filter(r => r.categoryId === i.id)
+          .filter(r => r.type === "outcome")
+          .reduce((total, record) => {
+            return total += +record.amount
+          },0)
+
+          const percent = 100 * spend / i.limit
+          const progressPercent = percent > 100 ? 100 : percent
+          const progressColor = percent <  60 ? 'green' : percent < 100 ? 'yellow' : 'red'
+          
+          return {
+            ...i,
+            progressPercent,
+            progressColor,
+            spend
+          }
+    } )
+    
+    console.log('planning')
+    
+    this.loading = false
+  },
+}
+</script>
--- src/views/Record.vue
+++ src/views/Record.vue
@@ -104,7 +104,7 @@ export default {
         if(this.type === "income") {
           return true
         }
-
+  
         return this.info.bill >= this.amount
     }
   },
